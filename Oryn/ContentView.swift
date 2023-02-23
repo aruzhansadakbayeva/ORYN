431e58ba888c5b97f6d2f343215e09e56cd5118c
Aruzhan Sadakbayeva <aruuunna.ss@gmail.com>
2023-02-23 16:26:37 +0600

    .

--- Oryn/ContentView.swift
+++ Oryn/ContentView.swift
@@ -1,1 +1,17 @@
 
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+Text("Hello")
+        }
+    }
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
